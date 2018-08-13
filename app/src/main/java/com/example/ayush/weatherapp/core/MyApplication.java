aeee52e0638198bad7d14ebe9021036e13354537
PolyMorpher01 <ayushshta@gmail.com>
2018-08-13 12:15:56 +0545

    add librabry dependencies

---
+++ app/src/main/java/com/example/ayush/weatherapp/core/MyApplication.java
@@ -0,0 +1,17 @@
+package com.example.ayush.weatherapp.core;
+
+import android.app.Application;
+import com.example.ayush.weatherapp.BuildConfig;
+import timber.log.Timber;
+import timber.log.Timber.DebugTree;
+
+public class MyApplication extends Application {
+
+  @Override public void onCreate() {
+    super.onCreate();
+
+    if (BuildConfig.DEBUG) {
+      Timber.plant(new DebugTree());
+    }
+  }
+}
