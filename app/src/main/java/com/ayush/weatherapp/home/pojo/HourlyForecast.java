81fa70a97df4c8554f1286e0e7c0e85e963d9b1f
PolyMorpher01 <ayushshta@gmail.com>
2018-08-17 11:08:45 +0545

    WEA-6 Remove unnecessary parameters

--- app/src/main/java/com/ayush/weatherapp/home/pojo/HourlyForecast.java
+++ app/src/main/java/com/ayush/weatherapp/home/pojo/HourlyForecast.java
@@ -24,19 +24,8 @@ public class HourlyForecast {
     @SerializedName("time") private int time;
     @SerializedName("summary") private String summary;
     @SerializedName("icon") private String icon;
-    @SerializedName("precipIntensity") private float precipIntensity;
-    @SerializedName("precipProbability") private float precipProbability;
     @SerializedName("temperature") private float temperature;
     @SerializedName("apparentTemperature") private float apparentTemperature;
-    @SerializedName("dewPoint") private float dewPoint;
-    @SerializedName("humidity") private float humidity;
-    @SerializedName("pressure") private float pressure;
-    @SerializedName("windSpeed") private float windSpeed;
-    @SerializedName("windBearing") private float windBearing;
-    @SerializedName("cloudCover") private float cloudCover;
-    @SerializedName("uvIndex") private float uvIndex;
-    @SerializedName("visibility") private float visibility;
-    @SerializedName("ozone") private float ozone;
 
     public int getTime() {
       return time;
@@ -50,14 +39,6 @@ public class HourlyForecast {
       return icon;
     }
 
-    public float getPrecipIntensity() {
-      return precipIntensity;
-    }
-
-    public float getPrecipProbability() {
-      return precipProbability;
-    }
-
     public float getTemperature() {
       return temperature;
     }
@@ -66,41 +47,6 @@ public class HourlyForecast {
       return apparentTemperature;
     }
 
-    public float getDewPoint() {
-      return dewPoint;
-    }
-
-    public float getHumidity() {
-      return humidity;
-    }
-
-    public float getPressure() {
-      return pressure;
-    }
-
-    public float getWindSpeed() {
-      return windSpeed;
-    }
-
-    public float getWindBearing() {
-      return windBearing;
-    }
-
-    public float getCloudCover() {
-      return cloudCover;
-    }
-
-    public float getUvIndex() {
-      return uvIndex;
-    }
-
-    public float getVisibility() {
-      return visibility;
-    }
-
-    public float getOzone() {
-      return ozone;
-    }
   }
 }
 
