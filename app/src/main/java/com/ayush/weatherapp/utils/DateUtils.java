e1727fe072b98f1a42804d6ac04cff1090e4cfa2
PolyMorpher01 <ayushshta@gmail.com>
2018-08-21 12:58:48 +0545

    WEA-6 Minor code refactor

--- app/src/main/java/com/ayush/weatherapp/utils/DateUtils.java
+++ app/src/main/java/com/ayush/weatherapp/utils/DateUtils.java
@@ -8,10 +8,12 @@ import static android.text.format.DateUtils.isToday;
 
 public final class DateUtils {
 
+  private static final String DAY_OF_THE_WEEK = "EE";
+  private static final String TIME = "hh:mm aa";
   private DateUtils() {
   }
 
-  public static  String getDayOfTheWeek(int timeStamp) {
+  public static String getDayOfTheWeek(int timeStamp) {
 
     if (isToday((long) timeStamp * 1000)) {
       return "Today";
@@ -19,12 +21,12 @@ public final class DateUtils {
 
     Date date = new Date((long) timeStamp * 1000);
 
-    return new SimpleDateFormat("EE", Locale.getDefault()).format(date);
+    return new SimpleDateFormat(DAY_OF_THE_WEEK, Locale.getDefault()).format(date);
   }
 
   public static String getTime(int timeStamp) {
     Date date = new Date((long) timeStamp * 1000);
 
-    return new SimpleDateFormat("hh:mm aa", Locale.getDefault()).format(date);
+    return new SimpleDateFormat(TIME, Locale.getDefault()).format(date);
   }
 }
--- app/src/main/java/com/ayush/weatherapp/utils/MathUtils.java
+++ app/src/main/java/com/ayush/weatherapp/utils/MathUtils.java
@@ -1,6 +1,9 @@
 package com.ayush.weatherapp.utils;
 
-public final class MiscUtils {
+public final class MathUtils {
+
+  private MathUtils() {
+  }
 
   public static double getAverage(double firstNum, double secondNum) {
     return (firstNum + secondNum) / 2;
