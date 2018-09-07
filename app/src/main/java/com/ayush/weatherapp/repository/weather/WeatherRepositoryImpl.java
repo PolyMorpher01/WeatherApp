298adf3c3228e3fd13578f46ad77f0e70da6ad86
PolyMorpher01 <ayushshta@gmail.com>
2018-09-07 17:05:42 +0545

    WEA-24 Minor code refactor

--- app/src/main/java/com/ayush/weatherapp/repository/weather/WeatherRepositoryImpl.java
+++ app/src/main/java/com/ayush/weatherapp/repository/weather/WeatherRepositoryImpl.java
@@ -1,19 +1,78 @@
 package com.ayush.weatherapp.repository.weather;
 
-import com.ayush.weatherapp.retrofit.weatherApi.WeatherAPIClient;
-import com.ayush.weatherapp.retrofit.weatherApi.WeatherAPIInterface;
+import com.ayush.weatherapp.mapper.ForecastDTOtoRealmMapper;
+import com.ayush.weatherapp.realm.RealmUtils;
+import com.ayush.weatherapp.realm.model.Forecast;
 import com.ayush.weatherapp.retrofit.weatherApi.pojo.ForecastDTO;
 import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import io.realm.Realm;
+import timber.log.Timber;
 
 public class WeatherRepositoryImpl implements WeatherRepository {
-  private WeatherAPIInterface weatherApiInterface;
+  private WeatherRepository onlineWeatherRepository;
+  private WeatherRepository localWeatherRepository;
 
-  // TODO provide dependencies using dagger
   public WeatherRepositoryImpl() {
-    weatherApiInterface = WeatherAPIClient.getClient().create(WeatherAPIInterface.class);
+    onlineWeatherRepository = new OnlineWeatherRepositoryImpl();
+    localWeatherRepository = new LocalWeatherRepositoryImpl();
   }
 
   @Override public Observable<ForecastDTO> getForecast(String coordinates) {
-    return weatherApiInterface.getForecast(coordinates);
+    //Observable<ForecastDTO> observable = Observable.create(emitter -> {
+    //  emitter.onNext();
+    //});
+    //onlineWeatherRepository.getForecast(coordinates)
+    //    .map(forecast -> {
+    //      // save to local db
+    //      return forecast;
+    //    })
+    //    .map(forecast -> {
+    //      // DTO to entity (view model ) mapper and send
+    //    }).
+
+    onlineWeatherRepository.getForecast(coordinates)
+        .map(forecastDTO -> {
+          Forecast forecast = ForecastDTOtoRealmMapper.transform(forecastDTO);
+          saveWeatherForecast(forecast);
+          return forecast;
+        })
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe();
+
+    return getRepository().getForecast(coordinates);
+  }
+
+  private WeatherRepository getRepository() {
+/*    if (isSavedLocally()) {
+      // todo check time
+      Timber.e("inside");
+      return localWeatherRepository;
+    }*/
+    Timber.e("outside");
+    return onlineWeatherRepository;
+  }
+
+  private boolean isSavedLocally() {
+    Realm realm = RealmUtils.getRealm();
+    boolean isSaved = realm.where(Forecast.class).count() > 0;
+    realm.close();
+    return isSaved;
+  }
+
+  private void saveWeatherForecast(Forecast forecast) {
+    removeAllWeatherForecast();
+
+    Realm realm = RealmUtils.getRealm();
+    realm.executeTransaction(r -> realm.insert(forecast));
+    realm.close();
+  }
+
+  private void removeAllWeatherForecast() {
+    Realm realm = RealmUtils.getRealm();
+    realm.executeTransaction(r -> r.deleteAll());
+    realm.close();
   }
 }
